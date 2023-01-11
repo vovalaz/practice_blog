6acf6c13a707f3f252edc44b1d654288bb476932
Volodymyr Lazarev <vovalaz321@gmail.com>
2023-01-11 16:11:37 +0200

    Task 1: created serializers for post and user models

---
+++ app/posts/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from posts.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'text_content', 'post_date' 'user_id']
+        extra_kwargs = {'user_id': {'read_only': True}}
