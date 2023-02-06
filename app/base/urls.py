caa3b8c7e7cd1ff567b28e4e1912899d953152e8
Volodymyr Lazarev <vovalaz321@gmail.com>
2023-02-06 17:24:32 +0200

    Task 5: added swagger documentation

--- app/base/urls.py
+++ app/base/urls.py
@@ -1,20 +1,35 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenBlacklistView,
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Vovalaz blog",
+        default_version="v1",
+        contact=openapi.Contact(email="vovalaz321@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny, ),
+)
 
 
 urlpatterns = [
+    path("", schema_view.with_ui("swagger", cache_timeout=0)),
     path("admin/", admin.site.urls),
     path("posts/", include("posts.urls")),
     path("users/", include("users.urls")),
     path("reactions/", include("reactions.urls")),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
 ]
